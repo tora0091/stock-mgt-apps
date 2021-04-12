4d204eb5cc3e1758fd7a2c59a85bd48b0f1f55aa
tora0091 <tora0091@yahoo.co.jp>
2021-04-12 21:29:50 +0900

    add server file

--- server/main.go
+++ server/main.go
@@ -3,15 +3,45 @@ package main
 import (
 	"net/http"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
+type Profile struct {
+	Id    int16  `json:"id"`
+	Title string `json:"title"`
+	Body  string `json:"body"`
+}
+
 func main() {
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowMethods: []string{
+			"POST",
+			"GET",
+		},
+		AllowHeaders: []string{
+			"Access-Control-Allow-Headers",
+			"Content-Type",
+			"Content-Length",
+			"Accept-Encoding",
+			"X-CSRF-Token",
+			"Authorization",
+		},
+		AllowOrigins: []string{
+			"http://localhost:3000",
+		},
+	}))
+
+	profile := []Profile{
+		Profile{Id: 1, Title: "title 1", Body: "body 1"},
+		Profile{Id: 2, Title: "title 2", Body: "body 2"},
+		Profile{Id: 3, Title: "title 3", Body: "body 3"},
+	}
+
 	r.GET("/api/v1/events", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "hello gin world.",
-		})
+		c.JSON(http.StatusOK, profile)
 	})
 	r.Run(":3001")
 }
